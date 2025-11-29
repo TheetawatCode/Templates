851e37f21fc8545e7369cf881ca73271c4aceb80
Theetawat Premsawat <theetawat.psw@gmail.com>
2025-11-30 01:39:31 +0700

    commit

--- NextJS/nextjs-template/src/app/(marketing)/blog/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-      <main className="max-w-3xl mx-auto py-10">
-        <h1 className="text-2xl font-semibold">This page is under construction.</h1>
-      </main>
-    );
-  }
-  
