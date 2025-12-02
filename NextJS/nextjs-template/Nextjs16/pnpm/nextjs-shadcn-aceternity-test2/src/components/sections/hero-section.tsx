eb9ff672b495365b59b8b1d2b62652fbcd144f38
Theetawat Premsawat <theetawat.psw@gmail.com>
2025-12-02 23:44:33 +0700

    commit

---
+++ NextJS/nextjs-template/Nextjs16/pnpm/nextjs-shadcn-aceternity-test2/src/components/sections/hero-section.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ThreeDCardDemo } from '../aceternity/3d-card-demo'
+import { Button } from '../ui/button'
+
+export default function HeroSection() {
+  return (
+    <div>
+      <h1>HeroSection</h1>
+      <ThreeDCardDemo />
+      <Button>Get Started</Button>
+    </div>
+  )
+}
