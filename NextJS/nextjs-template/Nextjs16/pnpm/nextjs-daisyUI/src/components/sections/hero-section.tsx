86b76092837d72fb7795844266e7e42c538e90ee
Theetawat Premsawat <theetawat.psw@gmail.com>
2025-12-05 13:07:29 +0700

    commit

---
+++ NextJS/nextjs-template/Nextjs16/pnpm/nextjs-daisyUI/src/components/sections/hero-section.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../ui/Navbar'
+
+
+export default function HeroSection() {
+  return (
+    <div>
+      <h1>HeroSection</h1>
+      <Navbar />
+
+    </div>
+  )
+}
